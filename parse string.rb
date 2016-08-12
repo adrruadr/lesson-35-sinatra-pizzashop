28bbf173a14f262c05ed44025ae57f2659f9c4ac
shyssirixius <ivmikvik@gmail.com>
2016-08-12 12:44:37 +0300

    parse_orders_line comlete

--- parse string.rb
+++ parse string.rb
@@ -1,12 +1,23 @@
-orders = "product_1=2,product_2=2,product_3=3,"
+orders_line = "product_1=5,product_2=6,product_3=7,"
 
-s1 = orders.split(',')
+def parse_orders_line orders_line
 
-s1.each do |x|
-	s2 = x.split('=')
-	
-		s3 = s2[0].split('_')
-		key = s3[1]
-		value = s3[1]	
+s1 = orders_line.split(',')
+arr = []
+
+	s1.each do |x|
+		s2 = x.split('=')
+		
+			s3 = s2[0].split('_')
+
+			id = s3[1]
+			cnt = s2[1]	
+
+			arr2 = [id, cnt]
+
+			arr.push arr2
+	end	
+return arr
 end
 
+puts parse_orders_line(orders_line).inspect
